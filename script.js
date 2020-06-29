940258be5454e92d65d82697c42b9e119f6bdec1
garshtoshteles <tj.yuxn.scott@gmail.com>
2020-06-29 19:21:15 -0400

    commit 5

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
   //
   //
+  //   need to get the current time
+  console.log("moment().format() : " + moment().format("kk:mm:ss"));
+  var currenthour = parseInt(moment().format("kk"));
   //
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
-
+  for (let i = 9; i <= 17; i++) {
+    //   create a new div for the timeblock
+    var newblock = $("<div>");
+    newblock.append(i + " </br>");
+    // assign a val attr to the div consisting of the index number
+    newblock.attr({ "data-time": i, class: "time-block" });
+    // does making the div color responsive go here?
+    $(".container").append(newblock);
+    //
+  }
 });
